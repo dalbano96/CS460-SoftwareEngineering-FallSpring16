cb01caec1d5b2bd926f938ab210dae79719c39fa
Daryl Albano <dalbano96@gmail.com>
2016-11-17 17:36:09 -1000

    Modified connect script

--- connect.sh
+++ connect.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Connecting to Team404 Server"
+IP_ADDR=104.236.140.186
 
-read -n1 -p "Root or someUser [1,2]" input
-case $input in
-	1) ssh root@138.68.50.53 ;;
-	2) ssh someUser@138.68.50.53 ;;
-	*) echo "Error, quiting now" ;;
-esac
+#read -n1 -p "Root or someUser [1,2]" input
+#case $input in
+#	1) ssh root@104.236.140.186 ;;
+#	2) ssh someUser@104.236.140.186 ;;
+#	*) echo "Error, quiting now" ;;
+#esac
+
+ssh root@$IP_ADDR
