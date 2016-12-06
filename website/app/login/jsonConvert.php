d78c888d94dc2d5299290790175afb4bf497be66
Daryl Albano <dalbano96@gmail.com>
2016-12-05 14:53:33 -1000

    Created a JSON converter file in php

---
+++ website/app/login/jsonConvert.php
@@ -0,0 +1,20 @@
+<?php
+	// Connect to MySQL db
+	require_once 'connect.php';
+	
+	// Prepare query to extract from users table
+	$query = "SELECT * FROM USERS";
+	$result = mysqli_query($link, $query) or die("Error, database not found: " .mysqli_error($link));
+
+	// Create array
+	$emptyArray = array();
+	while($row = mysqli_fetch_assoc($result)) {
+		emptyArray[] = $row;
+	}
+
+	// Convert array to JSON
+	echo json_encode($emptyArray);
+
+	// Close db connection
+	mysqli_close($link);
+?>
